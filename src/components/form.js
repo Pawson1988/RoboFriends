b53d42babed98a250709f732906953859f15598b
Pawson1988 <pawson1988@me.com>
2021-08-11 03:14:05 +0200

    just playing with forms

--- src/components/form.js
+++ src/components/form.js
@@ -1,19 +1,37 @@
+import React, {useState} from "react";
 
-function Form(){
-    
+function Form() {
 
-    return (
-       <form>
-           <label id="firstName">First Name:</label>
-           <input type="text" id="firstname" name="firstName" required/>
-           <label id="lastName">Last Name:</label>
-           <input type="text" id="lastName" name="lastName" required/>
-           <label id="age">Age:</label>
-           <input type="number" id="age" name="age" required/>
-           <button>Submit</button>
-       </form>
+const [input1, setInput1] = useState("nothing here.....");
+const [input2, setInput2] = useState("nothing here.....");
+const [input3, setInput3] = useState("nothing here.....");
 
-    )
+const inputField1 = (e) => {
+    setInput1(e.target.value)
+}
+const inputField2 = (e) => {
+    setInput2(e.target.value)
+}
+const inputField3 = (e) => {
+    setInput3(e.target.value)
+}
+
+  return (
+    <div>
+      <form>
+        <label id="firstName">First Name:</label>
+        <input onChange={inputField1} type="text" id="firstname" name="firstName" required />
+        <label id="lastName">Last Name:</label>
+        <input onChange={inputField2} type="text" id="lastName" name="lastName" required />
+        <label id="age">Age:</label>
+        <input onChange={inputField3} type="number" id="age" name="age" required />
+        <button>Submit</button>
+      </form>
+      <p>{input1}</p>
+      <p>{input2}</p>
+      <p>{input3}</p>
+    </div>
+  );
 }
 
 export default Form;
